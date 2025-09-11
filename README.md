6bdb8b36d2cc4ae02fc3f6c23f90feafcaa653f2
Denzou20202 <bugsbunny8432@gmail.com>
2025-09-11 18:00:09 +0300

    Done HW_1.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 1. Clean default project
 2. Creat new branch homework_tasks
+3. Creat new Lesson_1/HW_1.1
+4. Done HW_1.1
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import './components/coursesTitle.css'
+import coursesTitleArray from "./components/coursesTitle.tsx";
 
 function App() {
   return (
     <>
-
+      {
+        coursesTitleArray.map(value => <li>{value}</li>)
+      }
     </>
   )
 }
